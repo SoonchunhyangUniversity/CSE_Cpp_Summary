eeead8ad0d3c754bcd96808fad9422a65eab15e6
alstn2468 <alstn2468_@naver.com>
2019-04-06 16:51:57 +0900

    Init chapter 5

---
+++ Chapter_4/2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Computer
+{
+    string name;
+    int RAM;
+    double cpu_speed;
+
+    public:
+        void setComputer(string name, int RAM, double cpu_speed)
+        {
+            this->name = name;
+            this->RAM = RAM;
+            this->cpu_speed = cpu_speed;
+        }
+
+        void print()
+        {
+            cout << "이름 : " << this->name << endl;
+            cout << "RAM : " << this->RAM << endl;
+            cout << "CPU 속도 : " << this->cpu_speed << endl;
+        }
+};
+
+int main()
+{
+    Computer com;
+
+    com.setComputer("오피스컴퓨터", 8, 4.2);
+
+    com.print();
+
+    return 0;
+}
